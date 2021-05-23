1dcd32268a4f72001666bc1d96287d35e3a41a02
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2021-05-23 18:04:22 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// Copyright Henrik Widlund
+// Apache License 2.0
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HostsParser
+{
+    internal static class Program
+    {
+        internal static async Task Main(string[] args)
+        {
+            using var httpClient = new HttpClient();
+            using var httpResponseMessage = await httpClient.GetAsync("https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts");
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var lines = (await httpResponseMessage.Content.ReadAsStringAsync()).Split('\n');
+            
+            var headerLines = new List<string>(16){"# Content below this line is based on StevenBlack/hosts and only modified to work with AdGuard Home"};
+            headerLines.AddRange(lines[..14]);
+            headerLines.Add(string.Empty);
+
+            var skipLines = new string[14]
+            {
+                "127.0.0.1 localhost",
+                "127.0.0.1 localhost.localdomain",
+                "127.0.0.1 local",
+                "255.255.255.255 broadcasthost",
+                "::1 localhost",
+                "::1 ip6-localhost",
+                "::1 ip6-loopback",
+                "fe80::1%lo0 localhost",
+                "ff00::0 ip6-localnet",
+                "ff00::0 ip6-mcastprefix",
+                "ff02::1 ip6-allnodes",
+                "ff02::2 ip6-allrouters",
+                "ff02::3 ip6-allhosts",
+                "0.0.0.0 0.0.0.0"
+            };
+
+            var newLines = new HashSet<string>(headerLines);
+            lines = lines.Except(newLines).Except(skipLines).ToArray();
+
+            const string ipFilter = "0.0.0.0 ";
+            var length = ipFilter.Length;
+            foreach (var item in lines)
+            {
+                if (!item.StartsWith('#')&& item.StartsWith(ipFilter))
+                    newLines.Add(Replace(item, length));
+            }
+            
+            await File.WriteAllLinesAsync("hosts", newLines);
+        }
+
+        private static string Replace(ReadOnlySpan<char> item, int length)
+             => $"||{item.Slice(length).ToString()}^";
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# HostsParser
+[![Build](https://github.com/henrikwidlund/HostsParser/actions/workflows/build.yml/badge.svg)](https://github.com/henrikwidlund/HostsParser/actions/workflows/build.yml)
+[![Publish](https://github.com/henrikwidlund/HostsParser/actions/workflows/ci.yml/badge.svg)](https://github.com/henrikwidlund/HostsParser/actions/workflows/ci.yml)
+
+Converts [StevenBlack/hosts](https://github.com/StevenBlack/hosts) with fakenews, gambling and porn extensions into a format that works with [AdGuard Home](https://github.com/AdguardTeam/AdGuardHome). It also removes duplicates and most comments used to indicate different sections in the source.
+
+## Building
+*You'll need the [dotnet 5 SDK](https://dotnet.microsoft.com/download).*
+
+Run `dotnet build --configuration Release` from the directory you cloned the repository to.
+
+## Running
+*You'll need the [dotnet 5 runtime](https://dotnet.microsoft.com/download).*
+
+Run `dotnet HostsParser.dll`. Program creates a `hosts` file in the same directory.
