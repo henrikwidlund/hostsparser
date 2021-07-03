5c306ed809d9fb64360463ef3a57e1126ee19f85
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2021-07-03 16:50:29 +0200

    Use txt extension

--- Constants.cs
+++ Constants.cs
@@ -14,6 +14,6 @@
         internal const string LoopbackEntry = "0.0.0.0 0.0.0.0";
         internal const string WwwPrefix = "www.";
         internal const string UseExternalLastRun = "USE_EXTERNAL_LAST_RUN";
-        internal const string ModifiedFile = "MODIFIED2";
+        internal const string ModifiedFile = "MODIFIED.txt";
     }
 }
